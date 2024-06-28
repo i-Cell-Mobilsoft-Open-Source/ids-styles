452c3ea1625f5c7535e53088b689103b358eba35
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-06-28 19:49:32 +0200

    check missing tokens with file filter pattern

--- scripts/check-missing-tokens.mjs
+++ scripts/check-missing-tokens.mjs
@@ -1,49 +1,73 @@
-import { readFileSync, readdirSync } from "fs";
+import { readFileSync, readdirSync } from 'fs';
+import { program } from 'commander';
 
-const stylesRootPath = process.argv[2];
-const tokensFile = process.argv[3];
+program
+  .name('check-missing-tokens')
+  .version('1.0.0')
+  .description('Script to check missing tokens in CSS files.')
+  .arguments('<cssRootFolder> <tokenCssFile> [fileFilterPattern]')
+  .option('--limit <number>', 'Maximum number of missing tokens to log', parseInt)
+  .option('-v, --verbose', 'Enable verbose logging')
+  .action((cssRootFolder, tokenCssFile = '', fileFilterPattern = '') => {
+    const options = program.opts();
+    const limit = options.limit !== undefined ? options.limit : Infinity;
+    const verbose = options.verbose;
 
-if (process.argv.length < 4) {
-  throw new Error(
-    "Usage: node scripts/check-missing-tokens.mjs path/to/css/folder path/to/token/css/tokens.css <maxPrintableMissingtokensCount>"
-  );
-}
+    try {
+      const stylesDirents = readdirSync(cssRootFolder, { withFileTypes: true, recursive: true, encoding: 'utf-8' });
+      const cssDirents = stylesDirents
+        .filter((dirent) => dirent.path !== cssRootFolder)
+        .filter((dirent) => dirent.isFile() && /.min.css$/.test(dirent.name));
 
-const maxMissingTokenArg = parseInt(process.argv[4], 10);
+      let filteredCssDirents = [];
 
-const stylesDirents = readdirSync(stylesRootPath, { withFileTypes: true, recursive: true, encoding: "utf-8" });
-const cssDirents = stylesDirents.filter((dirent) => dirent.isFile() && /.min.css$/.test(dirent.name));
+      if (fileFilterPattern) {
+        filteredCssDirents = cssDirents.filter((dirent) => {
+          const fileFullName = `${dirent.path}/${dirent.name}`;
+          return new RegExp(fileFilterPattern).test(fileFullName);
+        });
 
-const tokenFileContent = readFileSync(tokensFile, { encoding: "utf-8" });
-const tokenRegExp = /--(-|\w|\d)+(?=:.*;)/g;
-const tokenSet = new Set(tokenFileContent.match(tokenRegExp));
+        if (filteredCssDirents.length === 0) {
+          throw new Error('No matching style file found.');
+        }
+      }
 
-const missingTokenList = [];
+      const tokenFileContent = readFileSync(tokenCssFile, { encoding: 'utf-8' });
+      const tokenRegExp = /--(-|\w|\d)+(?=:.*;)/g;
+      const tokenSet = new Set(tokenFileContent.match(tokenRegExp));
 
-const cssVariableRegExp = /(?<=var\()(-|\w|\d)+(?=\))/g;
+      const missingTokenList = [];
 
-cssDirents.forEach((dirent) => {
-  const fileFullName = `${dirent.path}/${dirent.name}`;
-  const cssFileContent = readFileSync(fileFullName, { encoding: "utf-8" });
-  const usedCssVariableList = cssFileContent.match(cssVariableRegExp);
+      const cssVariableRegExp = /(?<=var\()(-|\w|\d)+(?=\))/g;
 
-  usedCssVariableList.forEach((cssVariable) => {
-    const tokenExists = tokenSet.has(cssVariable);
-    if (!tokenExists) {
-      missingTokenList.push(cssVariable);
+      const files = fileFilterPattern ? filteredCssDirents : cssDirents;
+
+      files.forEach((dirent) => {
+        const fileFullName = `${dirent.path}/${dirent.name}`;
+        const cssFileContent = readFileSync(fileFullName, { encoding: 'utf-8' });
+        const usedCssVariableList = cssFileContent.match(cssVariableRegExp);
+
+        usedCssVariableList.forEach((cssVariable) => {
+          const tokenExists = tokenSet.has(cssVariable);
+          if (!tokenExists) {
+            missingTokenList.push(cssVariable);
+          }
+        });
+      });
+
+      const missingTokensCount = missingTokenList.length;
+      if (missingTokensCount > 0) {
+        const printableMissingTokens =
+          missingTokensCount <= limit || limit === Infinity
+            ? missingTokenList
+            : [...missingTokenList.slice(0, limit), `...and ${missingTokensCount - limit} more.`];
+        throw new Error(`${missingTokensCount} tokens are missing` + '\n' + printableMissingTokens.join('\n'));
+      }
+
+      console.info('Great! No tokens are missing.');
+    } catch (error) {
+      console.error(error.message);
     }
   });
-});
 
-const missingTokensCount = missingTokenList.length;
-if (missingTokensCount > 0) {
-  const MAX_PRINTABLE_MISSING_TOKENS = isNaN(maxMissingTokenArg) ? 1000 : maxMissingTokenArg;
-
-  const printableMissingTokens =
-    missingTokensCount <= MAX_PRINTABLE_MISSING_TOKENS
-      ? missingTokenList
-      : [...missingTokenList.slice(0, MAX_PRINTABLE_MISSING_TOKENS), `...and ${missingTokensCount - MAX_PRINTABLE_MISSING_TOKENS} more.`];
-  throw new Error(`${missingTokensCount} tokens are missing` + "\n" + printableMissingTokens.join("\n"));
-}
-
-console.info("Great! No tokens are missing.");
+program.parse(process.argv);
